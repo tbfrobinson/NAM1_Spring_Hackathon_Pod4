47c826630d064d29ab63e99224f3c8051336c955
Theo <tbfrobinson@gmail.com>
2023-06-26 13:01:09 -0700

    automatically seed pets

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const db = require('./models')
 
+const seedPets = async () => {
+    try {
+        await db.Pet.deleteMany({});
+        console.log('Deleted all pets');
+
+        const pets = [
+            {
+                category: 'Dog'
+            },
+            {
+                category: 'Cat'
+            },
+            {
+                category: 'Bird'
+            },
+            {
+                category: 'Fish'
+            },
+            {
+                category: 'Reptile'
+            },
+            {
+                category: 'Other'
+            },
+        ];
+
+        await db.Pet.insertMany(pets);
+        console.log('Created pets!');
+
+    } catch (err) {
+        console.log(err);
+        process.exit();
+    }
+}
+seedPets();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
